813d0dec2f96808918d40c3f07b0bf0e46464892
mohanmohadikar <chetandxg@gmail.com>
2019-04-30 00:25:18 +0530

    basic version
    basic version.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from Blockchain import *
+import datetime as datetime
 
 
-
+obj = BlockChain()
 app = Flask(__name__)
 
 app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
@@ -17,8 +19,13 @@ def messageRecived():
 
 @socketio.on( 'my event' )
 def handle_my_custom_event( json ):
+  obj.add_block(Block(0,str(datetime.datetime.now()),str(json)))
   print( 'recived my event: ' + str( json ) )
   socketio.emit( 'my response', json, callback=messageRecived )
+  for b in obj.chain:
+      print("******************************")
+      print(b)
+      print("******************************")
 
 if __name__ == '__main__':
   socketio.run( app, debug = True )
